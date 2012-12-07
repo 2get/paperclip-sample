77f06bbd3e39a4e9121aa549f581cfee7625dbce
Mayoto Yoshida <2getjp@gmail.com>
2012-12-07 16:41:25 +0900

    complete upload photo development

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,7 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :photo_file_name
+  attr_accessible :photo
+  has_attached_file :photo,
+  :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+  :url => "/system/:attachment/:id/:style/:filename",
+  :styles => { :medium => '300x300>', :thumb => '100x100>' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 PaperclipSample::Application.routes.draw do
-  resources :photos
+  resources :photos, :except => :new
 
 
   # The priority is based upon order of creation:
@@ -51,7 +51,7 @@ PaperclipSample::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'photos#index'
 
   # See how all your routes lay out with "rake routes"
 
